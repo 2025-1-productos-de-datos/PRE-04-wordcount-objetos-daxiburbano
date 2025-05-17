020117e76805a0396ad8c3dc998841d268b1c426
Ximena Burbano <daburbanoo@unal.edu.co>
2025-05-17 08:34:10 -0500

    04_wordcount_obj_sinRefac

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -7,33 +7,19 @@ import subprocess
 def test_homework():
     """Test Word Count"""
 
-    if not os.path.exists("homework/src/"):
-        raise Exception("homework/src/ directory does not exist")
+    # Carpetas y archivos del paquete homework
 
-    if not os.path.exists("homework/src/_internals/"):
-        raise Exception("homework/src/_internals/ directory does not exist")
-
-    if not os.path.exists("homework/src/_internals/argument_parser.py"):
-        raise Exception("homework/src/_internals/argument_parser.py does not exist")
-
-    if not os.path.exists("homework/src/_internals/factory.py"):
-        raise Exception("homework/src/_internals/factory.py does not exist")
-
-    if not os.path.exists("homework/src/_internals/folder_manager.py"):
-        raise Exception("homework/src/_internals/folder_manager.py does not exist")
-
-    if not os.path.exists("homework/src/_internals/result_saver.py"):
-        raise Exception("homework/src/_internals/result_saver.py does not exist")
-
-    if not os.path.exists("homework/src/_internals/word_count_process.py"):
-        raise Exception("homework/src/_internals/word_count_process.py does not exist")
-
-    if not os.path.exists("homework/src/_internals/word_counter.py"):
-        raise Exception("homework/src/_internals/word_counter.py does not exist")
-
-    if not os.path.exists("homework/src/main.py"):
-        raise Exception("homework/src/main.py does not exist")
-
+    for path in [
+        "homework/src",
+        "homework/src/_internals",
+        "homework/src/_internals/count_words.py",
+        "homework/src/_internals/preprocess_lines.py",
+        "homework/src/_internals/read_all_lines.py",
+        "homework/src/_internals/split_into_words.py",
+        "homework/src/_internals/write_word_counts.py",
+    ]:
+        if not os.path.exists(path):
+            raise Exception(f"'{path}' directory does not exist")
     try:
         subprocess.run(
             ["python3", "-m", "homework", "data/input", "data/output"],
@@ -45,7 +31,7 @@ def test_homework():
     if not os.path.exists("data/output/"):
         raise Exception("'data/output/' directory does not exist")
 
-    results_file = "data/output/results.tsv"
+    results_file = "data/output/wordcount.tsv"
     if not os.path.exists(results_file):
         raise Exception(f"'{results_file}' file does not exist")
 
@@ -61,3 +47,66 @@ def test_homework():
     assert result.get("by", 0) == 3, "Incorrect count for 'by'"
     assert result.get("algorithms", 0) == 2, "Incorrect count for 'algorithms'"
     assert result.get("analysis", 0) == 4, "Incorrect count for 'analysis'"
+
+
+# import os
+# import subprocess
+
+
+# def test_homework():
+#     """Test Word Count"""
+
+#     if not os.path.exists("homework/src/"):
+#         raise Exception("homework/src/ directory does not exist")
+
+#     if not os.path.exists("homework/src/_internals/"):
+#         raise Exception("homework/src/_internals/ directory does not exist")
+
+#     if not os.path.exists("homework/src/_internals/argument_parser.py"):
+#         raise Exception("homework/src/_internals/argument_parser.py does not exist")
+
+#     if not os.path.exists("homework/src/_internals/factory.py"):
+#         raise Exception("homework/src/_internals/factory.py does not exist")
+
+#     if not os.path.exists("homework/src/_internals/folder_manager.py"):
+#         raise Exception("homework/src/_internals/folder_manager.py does not exist")
+
+#     if not os.path.exists("homework/src/_internals/result_saver.py"):
+#         raise Exception("homework/src/_internals/result_saver.py does not exist")
+
+#     if not os.path.exists("homework/src/_internals/word_count_process.py"):
+#         raise Exception("homework/src/_internals/word_count_process.py does not exist")
+
+#     if not os.path.exists("homework/src/_internals/word_counter.py"):
+#         raise Exception("homework/src/_internals/word_counter.py does not exist")
+
+#     if not os.path.exists("homework/src/main.py"):
+#         raise Exception("homework/src/main.py does not exist")
+
+#     try:
+#         subprocess.run(
+#             ["python3", "-m", "homework", "data/input", "data/output"],
+#             check=True,
+#         )
+#     except subprocess.CalledProcessError as e:
+#         raise Exception(f"Error running the homework script: {e}")
+
+#     if not os.path.exists("data/output/"):
+#         raise Exception("'data/output/' directory does not exist")
+
+#     results_file = "data/output/results.tsv"
+#     if not os.path.exists(results_file):
+#         raise Exception(f"'{results_file}' file does not exist")
+
+#     with open(results_file, "r", encoding="utf-8") as f:
+#         lines = f.readlines()
+#         result = {}
+#         for line in lines:
+#             key, value = line.strip().split("\t")
+#             result[key] = int(value)
+
+#     assert result.get("analytics", 0) == 5, "Incorrect count for 'analytics'"
+#     assert result.get("business", 0) == 7, "Incorrect count for 'business'"
+#     assert result.get("by", 0) == 3, "Incorrect count for 'by'"
+#     assert result.get("algorithms", 0) == 2, "Incorrect count for 'algorithms'"
+#     assert result.get("analysis", 0) == 4, "Incorrect count for 'analysis'"
